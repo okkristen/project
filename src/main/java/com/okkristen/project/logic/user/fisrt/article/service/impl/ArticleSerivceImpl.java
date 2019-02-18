cd3b209c75bad312a17a35031220246fdfaad5c4
ok克里斯汀 <402605042@qq.com>
2019-02-18 14:49:58 +0800

    添加 文章查询接口

--- src/main/java/com/okkristen/project/logic/user/fisrt/article/service/impl/ArticleSerivceImpl.java
+++ src/main/java/com/okkristen/project/logic/user/fisrt/article/service/impl/ArticleSerivceImpl.java
@@ -3,12 +3,14 @@ package com.okkristen.project.logic.user.fisrt.article.service.impl;
 import com.okkristen.project.core.service.impl.CommonServiceImpl;
 import com.okkristen.project.logic.user.fisrt.article.dto.ArticleDTO;
 import com.okkristen.project.logic.user.fisrt.article.entity.Article;
-import com.okkristen.project.logic.user.fisrt.article.service.ArtileSerivce;
+import com.okkristen.project.logic.user.fisrt.article.service.ArticleSerivce;
+import org.springframework.stereotype.Service;
 
 /**
  * @author ysj
  * @create 2018-12-27
  **/
-public class ArticleSerivceImpl extends CommonServiceImpl<Article,ArticleDTO>  implements ArtileSerivce {
+@Service
+public class ArticleSerivceImpl extends CommonServiceImpl<Article,ArticleDTO>  implements ArticleSerivce {
 
 }
--- src/main/java/com/okkristen/project/logic/user/fisrt/tag/controller/TagController.java
+++ src/main/java/com/okkristen/project/logic/user/fisrt/tag/controller/TagController.java
@@ -10,12 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 /**
  * @author ysj
  * @create 2018-12-27
  **/
+@RestController
+@RequestMapping("/api/tag")
 public class TagController extends BaseController<Tag> {
     @Autowired
     private DatabaseFiledService service;
